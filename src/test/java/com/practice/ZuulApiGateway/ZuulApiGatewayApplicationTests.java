6b67b4a14be7c08fcd5b80cd7ee2a3bc175ff54d
Raman Madaan <raman.madaan@wipro.com>
2020-06-11 08:27:30 +0100

    first push

---
+++ src/test/java/com/practice/ZuulApiGateway/ZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.ZuulApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
